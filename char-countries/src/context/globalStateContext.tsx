b692c589ae25d4d034f1ebe519ec170c39345f89
robsonffsantos <robson.2501@hotmail.com>
2022-10-07 18:30:42 -0300

    feat: country list added

--- char-countries/src/context/globalStateContext.tsx
+++ char-countries/src/context/globalStateContext.tsx
@@ -8,6 +8,21 @@ export const GlobalStateContext = createContext({} as UserContextType)
 export const UserProvider = ({ children }: UserContextProps) => {
   let [profile, setProfile] = useState<Profile[]>([])
   const [page, setPage] = useState<number>(1)
+  const countries = {
+    'ARG': ['BOL', 'BRA', 'CHL', 'PRY', 'URY'],
+    'BRA': ['ARG', 'BOL', 'COL', 'GUF', 'GUY', 'PRY', 'PER', 'SUR', 'URY', 'VEN'],
+    'BOL': ['ARG', 'BRA', 'CHL', 'PRY', 'PER'],
+    'CHL': ['ARG', 'BOL', 'PER'],
+    'COL': ['BRA', 'ECU', 'PAN', 'PER', 'VEN'],
+    'ECU': ['COL', 'PER'],
+    'GUF': ['BRA', 'SUR', 'VEN'],
+    'PAN': ['COL'],
+    'PRY': ['ARG', 'BOL', 'BRA'],
+    'PER': ['BOL', 'BRA', 'CHL', 'COL', 'ECU'],
+    'SUR': ['BRA', 'GUF', 'GUY'],
+    'VEN': ['COL', 'BRA', 'GUF'],
+    'URU': ['ARG', 'BRA'] 
+  }
 
   const getProfile = async() => {
       await axios.get(`${BASE_URL}/${page}`)
