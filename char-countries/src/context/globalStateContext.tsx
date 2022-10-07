ae43f99613e80a6c8af40d2750589296ea008027
robsonffsantos <robson.2501@hotmail.com>
2022-10-07 16:47:41 -0300

    feat: import from api

--- char-countries/src/context/globalStateContext.tsx
+++ char-countries/src/context/globalStateContext.tsx
@@ -1,63 +1,25 @@
-import { createContext, useState, useContext, useMemo, useEffect } from "react"
-import { UserContextProps, UserContextType, Pokemon } from '../types/types'
+import { createContext, useState, useContext } from "react"
+import { UserContextProps, UserContextType, Profile } from '../types/types'
 import { BASE_URL } from "../constants/url"
 import axios from "axios"
 
 export const GlobalStateContext = createContext({} as UserContextType)
 
 export const UserProvider = ({ children }: UserContextProps) => {
-  let [pokemonIdArray, setPokemonIdArray] = useState<number[]>([])
-  let min: number = 1
-  let max: number = 889
-  const [wordTyped, setWordTyped] = useState<string>('')
-  let [pokemon, setPokemon] = useState<Pokemon[]>([])
+  let [profile, setProfile] = useState<Profile[]>([])
+  const [page, setPage] = useState<number>(1)
 
-  const getRandomNumber = () => {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
-  }
-  
-  const fillArray = () => {
-    pokemonIdArray = []
-    pokemon = []
-    const gamesId = pokemonIdArray
-    for (let i = 0; i <= 4; i++) {
-      let x = getRandomNumber()
-      gamesId.push(x)
-    }
-
-    setPokemonIdArray(gamesId)
-  }
-
-  const addPokemon = async () => {
-    let x = getRandomNumber()
-
-    const {data} = await axios.get(`${BASE_URL}/${x}`)
-      setPokemon([...pokemon, data])
-  }
-
-  const sortPokemon = () => {
-    const sortPokemon = pokemon.sort(function(a, b){return 0.5 - Math.random()})
-    setPokemon(sortPokemon)
-  }
-
-  const getPokemon = async() => {
-    const catchPokemon = pokemon
-    for (let i = 0; i < pokemonIdArray.length; i++) {
-      await axios.get(`${BASE_URL}/${pokemonIdArray[i]}`)
+  const getProfile = async() => {
+      await axios.get(`${BASE_URL}/${page}`)
       .then((res) => {
-        catchPokemon.push(res.data)
+          setProfile(res.data)
       })
       .catch((error) => {
         console.log(error)
       })
     }
 
-    setPokemon(catchPokemon)
-  }
-
-  const variables = { pokemon, wordTyped, pokemonIdArray, fillArray, getPokemon, setWordTyped, addPokemon, sortPokemon }
+  const variables = { profile, setProfile, getProfile, setPage, page }
 
   return (
     <GlobalStateContext.Provider value={variables}>
--- char-countries/src/pages/char/char.tsx
+++ char-countries/src/pages/char/char.tsx
@@ -1,9 +1,31 @@
+import { useEffect } from "react"
+import { useUser } from "../../context/GlobalStateContext"
+import { Profile } from "../../types/types"
+import { MainBox } from "../components/mainBox"
+import { MainContainer } from "../components/MainCOntainer"
+import { useNavigate } from "react-router-dom"
 
 const Char = () => {
+    const { getProfile, profile } = useUser()
+    const history = useNavigate()
+
+    useEffect(() => {
+        getProfile()
+    }, [])
+
     return (
-        <div>
-            Char
-        </div>
+        <MainContainer>
+            <MainBox>
+                {profile.map((user: Profile) => {
+                    return <div>
+                        <img src={user.photo} />
+                        <div>{user.firstName}</div>
+                        <div>{user.lastName}</div>
+                        <div>{user.email}</div>
+                    </div>                    
+                })}
+            </MainBox>
+        </MainContainer>
     )
 }
 
